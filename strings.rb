2a32fe3863c353445fe21e9d0c0bd1898f236d5b
sev2109 <sara.e.vogel@gmail.com>
2016-03-28 19:49:51 -0400

    added strings program

---
+++ strings.rb
@@ -0,0 +1,8 @@
+def strings(words)
+	words.split
+end
+
+strings ("about me academia agile development bios Bootcamp Chris Stein collaboration Contexts Digital Fellows digitalhumanities discussion diversity education English Ethan Marcotte GCDI Get Real guest speakers independent study interactive intersectionality ITP 2011 ITP 2016 ITP students Jason Santa Maria learning lightning talks memory NYCDH Office Hours Omeka online Practicalities project development project proposals responsive web design scope creep start-up culture teaching technology Tom Kuhlmann user experience waterfall method Wikipedia workshops")
+
+
+
