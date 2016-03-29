708594e2b91bd30ae21d4a8c8995c8a9e26724bc
sev2109 <sara.e.vogel@gmail.com>
2016-03-28 22:18:59 -0400

    fixed the strings program!

--- strings.rb
+++ strings.rb
@@ -1,8 +1,9 @@
 def strings(words)
 	array = words.split
+		array.collect do |word|
+		puts "\"#{word}\""
+		end
 end
 
 strings ("about me academia agile development bios Bootcamp Chris Stein collaboration Contexts Digital Fellows digitalhumanities discussion diversity education English Ethan Marcotte GCDI Get Real guest speakers independent study interactive intersectionality ITP 2011 ITP 2016 ITP students Jason Santa Maria learning lightning talks memory NYCDH Office Hours Omeka online Practicalities project development project proposals responsive web design scope creep start-up culture teaching technology Tom Kuhlmann user experience waterfall method Wikipedia workshops")
-
-
 
