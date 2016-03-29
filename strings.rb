249c0572473040b213fac3faa52d83ccebe81c9a
sev2109 <sara.e.vogel@gmail.com>
2016-03-28 22:47:08 -0400

    with user input

--- strings.rb
+++ strings.rb
@@ -1,9 +1,10 @@
-def strings(words)
+def strings
+	puts "Hello! What words would you like to turn into individual strings? Paste now."
+	words = gets
 	array = words.split
 		array.collect do |word|
 		puts "\"#{word}\","
 		end
 end
 
-strings ("archive art Capitalism commodity biography Copyright Creative Commons critical pedagogy cyborg Cyborg Manifesto database Dialectic digital collection digital humanities Economic Structure Educational Technology embodiment eversion evolution Feminism film Free Software games gender Haraway HOWTO information internet Irony knowledge Labor learning Machines Marx N. Katherine Hayles Nakamura New Media open source pedagogy posthuman Power Structure race sharing Technology Twitter Wikipedia about me academia agile development bios Bootcamp Chris Stein collaboration Contexts Digital Fellows digitalhumanities discussion diversity education English Ethan Marcotte GCDI Get Real guest speakers independent study interactive intersectionality ITP 2011 ITP 2016 ITP students Jason Santa Maria learning lightning talks memory NYCDH Office Hours Omeka online Practicalities project development project proposals responsive web design scope creep start-up culture teaching technology Tom Kuhlmann user experience waterfall method Wikipedia workshops")
-
+strings
